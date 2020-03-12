af51516c34d81d8717d4351562fa40b671edd086
ImJustAMan <t912272092@sina.com>
2020-03-12 16:54:37 +0800

    fix: change object key

--- app/table/video.table.ts
+++ app/table/video.table.ts
@@ -6,7 +6,7 @@ module.exports = function(Sequelize) {
     video_name: STRING(30),
     video_path: STRING(100),
     created_at: DATE,
-    update_at: DATE,
+    updated_at: DATE,
     visitor_number: INTEGER
   };
 };
