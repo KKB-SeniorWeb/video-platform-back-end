299b5a4a37445078370cd6952f59161a1d0d4835
cuixiaorui <xrcui@kaikeba.com>
2020-06-09 12:27:25 +0800

    chore(sequelizerc): use env variables to configure of sequelizerc

---
+++ database/config/config.js
@@ -0,0 +1,29 @@
+const { MYSQL_HOST, MYSQL_PORT } = process.env;
+
+module.exports = () => ({
+  development: {
+    username: 'root',
+    password: '123456',
+    database: 'video_platform_development',
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false
+  },
+  test: {
+    username: 'root',
+    password: '123456',
+    database: 'video_platform_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  },
+  production: {
+    username: 'root',
+    password: '123456',
+    database: 'video_platform',
+    host: MYSQL_HOST,
+    port: MYSQL_PORT,
+    dialect: 'mysql',
+    operatorsAliases: false
+  }
+});
