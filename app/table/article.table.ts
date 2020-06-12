c00ee9fd3ab608148e69d0cda375f0ec288a6315
yanyan <499504148@qq.com>
2020-06-12 18:20:55 +0800

    fix(article): modify table's article

--- app/table/article.table.ts
+++ app/table/article.table.ts
@@ -1,11 +1,11 @@
 module.exports = function(Sequelize) {
-  const { STRING, INTEGER, DATE } = Sequelize;
+  const { STRING, DATE } = Sequelize;
 
   return {
     id: { type: STRING(70), primaryKey: true },
     title: STRING(30),
     cover: STRING(100),
-    describe: INTEGER(100),
+    describe: STRING(100),
     content: STRING(),
     author_id: STRING(70),
     created_at: DATE,
