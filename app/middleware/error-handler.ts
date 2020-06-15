b7f0b5edaa9379058677856711c980255dc64b1c
cuixiaorui <cui_xiaorui@126.com>
2020-06-15 15:58:27 +0800

    test: add cnosole.log for debug user test e2e on the git actions env

--- app/middleware/error-handler.ts
+++ app/middleware/error-handler.ts
@@ -18,6 +18,7 @@ module.exports = () => {
       await next();
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+      console.trace(err);
       if (ctx.app.config.env !== 'unittest') ctx.app.emit('error', err, ctx);
 
       const status = err.status || 500;
