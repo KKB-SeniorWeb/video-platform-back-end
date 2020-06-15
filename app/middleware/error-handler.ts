113e6b24015c51c7b9e4a49c62bb16a8c1cc07ea
cuixiaorui <cui_xiaorui@126.com>
2020-06-15 18:03:51 +0800

    chore: comments debug logic

--- app/middleware/error-handler.ts
+++ app/middleware/error-handler.ts
@@ -18,7 +18,8 @@ module.exports = () => {
       await next();
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
-      console.trace(err);
+      // debug useful
+      // console.trace(err);
       if (ctx.app.config.env !== 'unittest') ctx.app.emit('error', err, ctx);
 
       const status = err.status || 500;
