dcef9b4c39def1b4677d4bc52d8e63ae51e1617b
yanyan <499504148@qq.com>
2020-08-14 15:14:49 +0800

    fix(egg-cors): egg-cors to configure

--- typings/config/plugin.d.ts
+++ typings/config/plugin.d.ts
@@ -14,6 +14,7 @@ import 'egg-schedule';
 import 'egg-static';
 import 'egg-jsonp';
 import 'egg-view';
+import 'egg-cors';
 import 'egg-swagger-doc';
 import 'egg-sequelize';
 import 'egg-validate';
@@ -33,6 +34,7 @@ declare module 'egg' {
     static?: EggPluginItem;
     jsonp?: EggPluginItem;
     view?: EggPluginItem;
+    cors?: EggPluginItem;
     swaggerdoc?: EggPluginItem;
     sequelize?: EggPluginItem;
     validate?: EggPluginItem;
